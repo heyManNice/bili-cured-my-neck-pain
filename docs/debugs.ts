286a51f94d36a5c08e1f625ae1382a3584b5bb0f
heyManNice <3031978990@qq.com>
2025-12-06 22:18:28 +0800

    docs：添加调试脚本

---
+++ docs/debugs.ts
@@ -0,0 +1,7 @@
+const $0 = document.createElement('div');
+
+// 保持视频窗口控件显示
+setInterval(() => { document.querySelector('.bpx-player-container')?.setAttribute('data-ctrl-hidden', 'false') }, 1000);
+
+// 当前控件按钮元素展开悬浮框
+$0.classList.add('bpx-state-show');
