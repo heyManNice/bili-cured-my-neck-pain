0f9c78b5fd5e35104cbbf1c803d6c0acf5516efe
heyManNice <3031978990@qq.com>
2025-12-06 20:31:28 +0800

    feat：主逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log('hello');
+/// <reference path="./global.d.ts" />
+
+import rotateBtnHtml from './htmls/bpx-player-ctrl-rotate.html';
+import {
+    WaitUntilElementReady,
+    InsertHtmlBeforeElement
+} from './utils.ts';
+
+async function main() {
+    const pipBtn = await WaitUntilElementReady('.bpx-player-ctrl-btn.bpx-player-ctrl-pip');
+
+    const beginTime = Date.now();
+    InsertHtmlBeforeElement(pipBtn, rotateBtnHtml);
+
+    console.log(`[B站治好了我的颈椎病] 已加载，耗时 ${Date.now() - beginTime} ms`);
+}
+
+main();
