f970452be516af78533ff066a82088c553732e88
XuanYu <94221632+XuanYu-github@users.noreply.github.com>
2025-12-13 01:43:54 +0800

    fix：修复了脚本在稍后再看页面不生效的问题

--- tools/build-for-tm.ts
+++ tools/build-for-tm.ts
@@ -16,6 +16,7 @@ export function build() {
 // @description  给B站PC网页版添加视频旋转功能，喜欢的话点点小星星哟~
 // @author       https://github.com/heyManNice
 // @match        https://www.bilibili.com/video/*
+// @match        https://www.bilibili.com/list/watchlater/*
 // @icon         https://heymannice.github.io/bili-cured-my-neck-pain/assets/bcmnp-logo.png
 // @grant        GM_addStyle
 // ==/UserScript==
